fc36762d4ec91bc0f02583bab4be43054c06cf5c
luxtmxw <luxtmxw@126.com>
2016-12-21 09:38:18 +0800

    12.21.1

---
+++ teacher/HomePage/XXENotificationCenter/Controller/SystemNotificationPubilshListViewContorllor.h
@@ -0,0 +1,13 @@
+//
+//  SystemNotificationPubilshListViewContorllor.h
+//  teacher
+//
+//  Created by codeDing on 16/12/20.
+//  Copyright © 2016年 XingXingEdu. All rights reserved.
+//
+
+#import "XXEBaseViewController.h"
+
+@interface SystemNotificationPubilshListViewContorllor : XXEBaseViewController
+
+@end
